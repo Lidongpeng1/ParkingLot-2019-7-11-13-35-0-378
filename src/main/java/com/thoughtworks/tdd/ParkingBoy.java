5191086de60113b4a61c44d633f9e529a15e5bc3
“your_uLiDongpeng1 <l>
2019-07-15 21:25:44 +0800

    class Parkingboy andclass ParkingLot is Refactoring completed

--- src/main/java/com/thoughtworks/tdd/ParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/ParkingBoy.java
@@ -9,34 +9,34 @@ public class ParkingBoy {
 
     public ParkingBoy() {}
 
-    public ParkingBoy(ParkingLot parkingLot) {
+    public ParkingBoy(ParkingLot parkingLot) {  //传入特定停车场的构造函数
         assignParkingLot(parkingLot);
     }
 
-    public ParkingBoy(List<ParkingLot> parkingLots) {
+    public ParkingBoy(List<ParkingLot> parkingLots) {  //传入停车场的List集合来构造函数
         this.parkingLots = parkingLots;
     }
 
-    public ParkingTicket park(Car car) {
+    public ParkingTicket park(Car car) {  //传入车的对象获取停车票作为返回值
         RuntimeException exception = null;
         for (ParkingLot parkingLot : parkingLots) {
             try {
-                return parkingLot.park(car);
+                return parkingLot.park(car);  //遍历所有的停车场，如果当前停车场能够停车则返回在parkingLot获取的parkingTicket
             } catch (IllegalStateException e) {
                 exception = e;
             }
         }
-        if (exception != null) {
+        if (exception != null) {   //如果捕获的异常不为空，则抛出异常
             throw exception;
         }
-        return null;
+        return null; //如果运行到了最后说明停车依然失败，返回空
     }
 
     public Car fetchCar(ParkingTicket ticket) { //凭停车票获取相应的汽车
         RuntimeException exception = null;
-        for (ParkingLot parkingLot : parkingLots) {
+        for (ParkingLot parkingLot : parkingLots) {  //循环遍历所有停车场
             try {
-                return parkingLot.fetchCar(ticket);
+                return parkingLot.fetchCar(ticket); //所有停车场都调用fetchCar方法，查找到正确的停车票才会取车
             } catch (IllegalArgumentException e) {
                 exception = e;
             }
@@ -48,8 +48,8 @@ public class ParkingBoy {
     }
 
     public void assignParkingLot(ParkingLot parkingLot) {
-        Objects.requireNonNull(parkingLot);
-        this.parkingLots.add(parkingLot);
+        Objects.requireNonNull(parkingLot); //  如果parkinglot为空直接判空
+        this.parkingLots.add(parkingLot); //在当前对象的parkingLots中添加当前停车场
     }
 
     protected List<ParkingLot> getParkingLots() {
